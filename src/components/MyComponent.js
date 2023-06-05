7904dde2bc1e931d455bb1aaf6bb608eca8459cb
Hieu1504 <hoanghieu1542003@gmail.com>
2023-06-05 15:56:56 +0700

    update state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,12 +8,33 @@ class MyComponent extends React.Component {
         age: 26
     };
 
+    handleClick = (event) => {
+        console.log(">>> click me my button");
+        console.log("random",)
+
+        this.setState({
+            name: 'Hieu',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+        // this.setState({
+        //     age: Math.floor((Math.random() * 100) + 1)
+        // })
+    }
+
+    handleOnMouseOver = (event) => {
+        // console.log(event.pageX)
+    }
+
     //jsx
     render() {
         return (
             <div>
-                
-                My name is {this.state.name} and I'm from {this.state.address}
+
+                My name is {this.state.name} and I'm  {this.state.age}
+                <button onMouseOver={this.handleOnMouseOver}>hover me</button>
+                <button onClick={this.handleClick}>click me</button>
+
             </div>
         );
     }
