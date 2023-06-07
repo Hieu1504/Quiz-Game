654f01b4e8e21a3744eff4a931e02f79b9b33cae
Hieu1504 <hoanghieu1542003@gmail.com>
2023-06-07 08:18:18 +0700

    reset code

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,20 +3,39 @@ import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
 class DisplayInfor extends React.Component {
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log(">>> call contructor : 1")
+        super(props);
+        this.state = {
+            isShowListUser: true
+        }
+
     }
+
+    componentDidMount() {
+        console.log('>>> call me component did mount')
+        setTimeout(() => { document.title = 'Eric & Hoi dan IT' }, 3000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('>>> call me component did update', this.props, prevProps)
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.lenght === 5) {
+                alert('you got 5 users')
+            }
+        }
+    }
+
     handleShowHide = () => {
         this.setState({
             isShowListUser: !this.state.isShowListUser
         })
     }
     render() {
+        console.log('>>> call me render')
         //destructuring array/object
         const { listUsers } = this.props; // object
-        console.log(listUsers);
         //props => viết tắt của properties
-        console.log(this.props)
         return (
             <div className='display-infor-container'>
                 {/* <img src={logo} /> */}
