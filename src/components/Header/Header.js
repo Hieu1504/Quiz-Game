a54af94d4eab985ea7eadb186bacedd9a7997056
Hieu1504 <hoanghieu1542003@gmail.com>
2023-06-07 10:09:45 +0700

    home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,8 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand> */}
+                <NavLink to='/' className='navbar-brand'>Hỏi Dân IT</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -23,9 +24,9 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Login</NavDropdown.Item>
+                            <NavDropdown.Item >Logout</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
