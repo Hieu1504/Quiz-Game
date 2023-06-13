7719abb4ddd11c8300713ed24d770956844783ca
Hieu1504 <hoanghieu1542003@gmail.com>
2023-06-13 12:42:11 +0700

    update paginate

---
+++ src/components/Admin/Content/TableUserPaginate.js
@@ -0,0 +1,89 @@
+import ReactPaginate from "react-paginate";
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+
+const TableUserPaginate = (props) => {
+    const { listUsers, pageCount } = props;
+    //const listUsers = props.listUsers;
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+        props.fetchListUsersWithPaginate(+ event.selected + 1);
+        console.log(`User requested page number ${event.selected}`);
+    };
+    return (
+        <>
+            <table className="table table-hover table-bordered ">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">username</th>
+                        <th scope="col">email</th>
+                        <th scope="col">role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            return (
+                                <tr key={`table - users - ${index}`}>
+                                    <td scope="row">{item.id}</td>
+                                    <td>{item.username}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.role}</td>
+                                    <td>
+                                        <button
+                                            className="btn btn-secondary"
+                                            onClick={() => props.handleClickBtnViewUser(item)}>
+                                            View
+                                        </button>
+                                        <button
+                                            className="btn btn-warning mx-3"
+                                            onClick={() => props.handleClickBtnUpdate(item)}>
+                                            Update
+                                        </button>
+                                        <button
+                                            className="btn btn-danger"
+                                            onClick={() => props.handleClickBtnDelete(item)}>
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                    {listUsers && listUsers.length === 0 &&
+                        <tr>
+                            <td colSpan={'4'}>Not found data</td>
+                        </tr>}
+                </tbody>
+            </table>
+            <div className="user-pagination">
+                <ReactPaginate
+                    nextLabel="Next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel="< Previous"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    renderOnZeroPageCount={null}
+                />
+            </div>
+        </>
+    )
+}
+
+export default TableUserPaginate;
